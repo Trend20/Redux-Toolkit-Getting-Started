bdb042d6e23110d3437224cd194dc4db5c0e0c6c
Trend20 <enockomondi305@gmail.com>
2022-12-04 11:48:50 +0300

    adding cart conditions

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,12 +1,16 @@
+import { useSelector, useDispatch } from 'react-redux';
 import productList from '../data/product.json'
 import '../styles/cart.scss'
 
 const Cart = () => {
+
+  const { cartProductIds } = useSelector((state) => state.cart);
+  const cartProductData = productList.products.filter((product) => cartProductIds.includes(product.id));
   return (
     <div className="cart">
-      <div className="cart-product">
+      {cartProductData.length > 0 && (<div className="cart-product">
         <h3 className="header">Items in cart</h3>
-        {productList.products.map((product) => (
+        {cartProductData.map((product) => (
           <div key={product.id} className="row">
             <img className="item-image" src={product.imageUrl} alt="product" />
 
@@ -23,13 +27,13 @@ const Cart = () => {
         <footer className="text-center">
           <button className="btn btn-primary">CHECKOUT</button>
         </footer>
-      </div>
+      </div>)}
 
-      <div className="text-center empty-cart">
+      {cartProductData.length < 1 && (<div className="text-center empty-cart">
         <i className="bi bi-cart3" />
         <p>Your cart is empty.</p>
         <p>You have not added any item to your cart.</p>
-      </div>
+      </div>)}
     </div>
   )
 }
